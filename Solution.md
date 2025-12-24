8a865055d4b39134f4e071d7efb461aec560003a
erase1657 <erase1657@naver.com>
2025-12-24 16:11:36 +0900

    테스트1

---
+++ Solution.md
@@ -0,0 +1,39 @@
+# 정답
+# 1
+* c, d, f
+# 2
+* x = 10, y = 5
+# 3
+* total_Count
+* userName
+* usCompleted
+# 4
+* a = 20, b =10
+* arr1[0] = 100, arr2[0] = 100
+# 5
+* b,d,f
+# 6
+* true
+* false
+* true
+# 7 
+* 0 null
+* 0 null
+* true null
+# 8 
+# 9
+* 타입간의 불일치가 발생 result의 타입을 Integer로 바꿔준다.
+# 10
+* import java.io.BufferedReader
+* import java.io.IOException
+* import java.io.InputStreamReader
+# 11
+* reader.readLine()
+# 12
+* Integer.parseInt(ageStr)
+# 13
+* i < 10
+* System.out.println(i++);
+# 14
+* 15
+# 15
---
+++ step2/src/project.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class project {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            
+        System.out.println("=== 입력 테스트 ===");
+        while(true){
+            System.out.print("입력 (종료: quit) > ");
+            String s = br.readLine();
+            
+            if(s.equals("quit")) {
+                System.out.println("프로그램을 종료합니다.");
+                br.close();
+                break;
+            }
+            System.out.println("입력한 값: " + s);
+        }
+    }
+}
